ca76a981c9f4bc18bce3885e5df31a61836e2219
vikassharma555 <vikassharma555@gmail.com>
2025-04-18 00:25:52 +0530

    GAS Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// CopyrightVIkasAuraProject
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//this is how often server updates client
+	NetUpdateFrequency = 100.f;
+}
